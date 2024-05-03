1efab4351555c9c35b6918076d3bf18adfeb9d60
Anastasiia Siutkina <sutckina.n@gmail.com>
2024-05-03 20:30:33 +0200

    iplemented logic to display the list of tasks

--- src/app/compoments/task/task-list/task-list.component.ts
+++ src/app/compoments/task/task-list/task-list.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { TaskService } from '../../../services/task.service';
+import { Task } from '../../../models/task';
+import { Observable , tap} from 'rxjs';
+import { Router } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [],
+  imports: [TableModule, ButtonModule, HttpClientModule],
+  providers: [TaskService],
   templateUrl: './task-list.component.html',
-  styleUrl: './task-list.component.scss'
+  styleUrl: './task-list.component.scss',
 })
-export class TaskListComponent {
+export class TaskListComponent implements OnInit {
+  private readonly taskSerivce = inject(TaskService);
 
+  tasks: Task[] = [];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.taskSerivce.getTasks().pipe(
+    ).subscribe({
+      next: value => {
+        this.tasks = value
+        console.log('tasks', value)
+      }
+    });
+  }
 }
--- src/app/models/task.ts
+++ src/app/models/task.ts
@@ -1,8 +1,8 @@
 import { Subtask } from "./subtask";
 
 export interface Task {
-    id: number;
-    title: string;
+    techId: number;
+    taskTitle: string;
     deadline: string; 
     subtasks: Subtask[];
     status: boolean;
