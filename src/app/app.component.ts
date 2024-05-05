26aecb0e7f37e1554255d22dfc0e1ef5ef01aaca
Anastasiia Siutkina <120054681+anastasimars@users.noreply.github.com>
2024-05-05 20:39:10 +0200

    Add scss for task list (#10)
    * added style and expanded rows to table task-list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TaskListComponent } from './compoments/task/task-list/task-list.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TaskListComponent ],
+  imports: [RouterOutlet, TaskListComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/compoments/task/task-list/task-list.component.ts
+++ src/app/compoments/task/task-list/task-list.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { TableModule } from 'primeng/table';
+import {
+  TableModule,
+  TableRowCollapseEvent,
+  TableRowExpandEvent,
+} from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { TaskService } from '../../../services/task.service';
 import { Task } from '../../../models/task';
-import { Observable , tap} from 'rxjs';
-import { Router } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-task-list',
   standalone: true,
-  imports: [TableModule, ButtonModule, HttpClientModule],
+  imports: [TableModule, ButtonModule],
   providers: [TaskService],
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss',
@@ -21,14 +23,37 @@ export class TaskListComponent implements OnInit {
   tasks: Task[] = [];
 
   constructor() {}
+  expandedRows: { [s: string]: boolean } = {};
 
   ngOnInit(): void {
-    this.taskSerivce.getTasks().pipe(
-    ).subscribe({
-      next: value => {
-        this.tasks = value
-        console.log('tasks', value)
-      }
+    this.loadTasks();
+  }
+  onRowExpand($event: TableRowExpandEvent) {
+    console.log('Row expanded:', $event.data);
+  }
+  onRowCollapse($event: TableRowCollapseEvent) {
+    console.log('Row collapsed:', $event.data);
+  }
+  collapseAll() {
+    this.expandedRows = {};
+    console.log('All rows collapsed');
+  }
+  expandAll() {
+    this.tasks.forEach((task) => {
+      this.expandedRows[task.techId] = true;
     });
+    console.log('All rows expanded');
+  }
+
+  loadTasks(): void {
+    this.taskSerivce
+      .getTasks()
+      .subscribe({
+        next: (value) => {
+          this.tasks = value;
+          console.log('tasks', value);
+        },
+        error: (err) => console.error('Error loading tasks:', err),
+      });
   }
 }
